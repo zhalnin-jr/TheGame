611b5096b434116a75829b6e7aea786093cd2c80
JulKhizhn <muskoshka@mail.ru>
2024-06-01 01:47:43 +0300

    UnitFactories
    опять редактирую

--- ChipiChapaGame/Assets/Scripts/Factories/UnitFactories.cs
+++ ChipiChapaGame/Assets/Scripts/Factories/UnitFactories.cs
@@ -1,10 +1,19 @@
-﻿public class UnitFactories
+﻿/// <summary>
+/// Фабричные методы для создания юнитов.
+/// </summary>
+public class UnitFactories
 {
+    /// <summary>
+    /// Один интерфейс на класс.
+    /// </summary>
     public interface IUnitFactory
     {
         Unit CreateUnit(string name);
     }
 
+    /// <summary>
+    /// Для создания Лёгкого Пехотинца.
+    /// </summary>
     public class LightUnitFactory : IUnitFactory
     {
         public Unit CreateUnit(string name)
@@ -12,7 +21,10 @@
             return new LightUnit(name);
         }
     }
-
+    
+    /// <summary>
+    /// Для создания Тяжёлого Пехотинца.
+    /// </summary>
     public class HeavyUnitFactory : IUnitFactory
     {
         public Unit CreateUnit(string name)
@@ -20,6 +32,10 @@
             return new HeavyUnit(name);
         }
     }
+
+    /// <summary>
+    /// Для создания Лучника.
+    /// </summary>
     public class ArcherUnitFactory : IUnitFactory
     {
         public Unit CreateUnit(string name)
@@ -28,6 +44,9 @@
         }
     }
 
+    /// <summary>
+    /// Для создания Мага.
+    /// </summary>
     public class MageUnitFactory : IUnitFactory
     {
         public Unit CreateUnit(string name)
@@ -35,6 +54,10 @@
             return new MageUnit(name);
         }
     }
+
+    /// <summary>
+    /// Для создания Врача.
+    /// </summary>
     public class HealerUnitFactory : IUnitFactory
     {
         public Unit CreateUnit(string name)
