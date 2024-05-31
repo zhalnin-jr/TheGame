124818ba8d25c6509923a01c6320e0eaf0f46163
JulKhizhn <muskoshka@mail.ru>
2024-06-01 02:30:32 +0300

    юниты, юниты, юниты
    Маг готов, пехотинцы тоже

--- ChipiChapaGame/Assets/Scripts/Units/HeavyUnit.cs
+++ ChipiChapaGame/Assets/Scripts/Units/HeavyUnit.cs
@@ -1,9 +1,19 @@
 ﻿using static Unit;
 
+/// <summary>
+/// Класс для описания тяжёлого пехотинца.
+/// </summary>
 public class HeavyUnit : Unit, IHealableUnit
 {
+    /// <summary>
+    /// Создание через конструктор с параметрами.
+    /// </summary>
     public HeavyUnit(string name) : base(name, 200, 10, 10, 2) { }
 
+    /// <summary>
+    /// Мржет быть вылеченным.
+    /// </summary>
+    /// <param name="amount"> - значение хилла.</param>
     public void Heal(int amount)
     {
         HealthPoints = System.Math.Min(HealthPoints + amount, base.HealthPoints);
--- ChipiChapaGame/Assets/Scripts/Units/MageUnit.cs
+++ ChipiChapaGame/Assets/Scripts/Units/MageUnit.cs
@@ -1,9 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+
+/// <summary>
+/// Описание юнита Маг.
+/// </summary>
 public class MageUnit : Unit
 {
+    /// <summary>
+    /// Создание юнита согласно конструктору с параметрами.
+    /// </summary>
     public MageUnit(string name) : base(name, 50, 30, 10, 3) { }
     private static readonly Random random = new Random();
+
+    /// <summary>
+    /// Метод для клонирования лёгкого юнита магом.
+    /// </summary>
+    /// <param name="mage"> - передаём мага.</param>
+    /// <param name="units"> - работаем со списком юнитов.</param>
     public void CloneAdjacentLightUnit(MageUnit mage, List<Unit> units)
     {
         int chance = random.Next(1, 101);
@@ -22,7 +35,7 @@ public class MageUnit : Unit
             {
                 unitToClone = rightUnit;
             }
-
+            // Клонируем.
             if (unitToClone != null && unitToClone is ICloneableUnit cloneable)
             {
                 var clonedUnit = cloneable.Clone();
